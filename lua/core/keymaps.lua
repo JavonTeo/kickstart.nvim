e61fbc27b2be654ec79d3da606dca5249de65847
JavonTeo <javonteo@hotmail.com>
2025-09-23 21:58:13 +0800

    Change terminal to configure tmux

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,13 +9,23 @@ vim.g.maplocalleader = ' '
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set(
+	'n',
+	'<leader>q',
+	vim.diagnostic.setloclist,
+	{ desc = 'Open diagnostic [Q]uickfix list' }
+)
 
 -- Resize current window
-vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', { desc = 'Resize current window up'})
-vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', { desc = 'Resize current window down'})
-vim.keymap.set('n', '<C-Left>', ':vertical resize +2<CR>', { desc = 'Resize current window left'})
-vim.keymap.set('n', '<C-Right>', ':vertical resize -2<CR>', { desc = 'Resize current window right'})
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', { desc = 'Resize current window up' })
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', { desc = 'Resize current window down' })
+vim.keymap.set('n', '<C-Left>', ':vertical resize +2<CR>', { desc = 'Resize current window left' })
+vim.keymap.set(
+	'n',
+	'<C-Right>',
+	':vertical resize -2<CR>',
+	{ desc = 'Resize current window right' }
+)
 
 -- Set options for keymaps
 local opts = { noremap = true, silent = true }
@@ -27,31 +37,42 @@ local opts = { noremap = true, silent = true }
 -- vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
 
 -- Window management
-vim.keymap.set('n', '<leader>v', '<C-w>v', opts) -- split window vertically
-vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- split window horizontally
-vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- make split windows equal width & height
+vim.keymap.set('n', '<leader>v', '<C-w>v', opts)      -- split window vertically
+vim.keymap.set('n', '<leader>h', '<C-w>s', opts)      -- split window horizontally
+vim.keymap.set('n', '<leader>se', '<C-w>=', opts)     -- make split windows equal width & height
 vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Toggle line wrapping
 vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!,CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', function()
-  vim.diagnostic.jump { count = -1, float = true }
+	vim.diagnostic.jump { count = -1, float = true }
 end, { desc = 'Go to previous diagnostic message' })
 
 vim.keymap.set('n', ']d', function()
-  vim.diagnostic.jump { count = 1, float = true }
+	vim.diagnostic.jump { count = 1, float = true }
 end, { desc = 'Go to next diagnostic message' })
 
-vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set(
+	'n',
+	'<leader>d',
+	vim.diagnostic.open_float,
+	{ desc = 'Open floating diagnostic message' }
+)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- File navigation settings
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
