bb96a20453b16f5be2bed2095a9b3d7a4966a535
JavonTeo <javonteo@hotmail.com>
2025-12-16 10:21:16 +0800

    Replace telescope with fzf

---
+++ lua/custom/plugins/fzf.lua
@@ -0,0 +1,72 @@
+return {
+	'ibhagwan/fzf-lua',
+	-- optional for icon support
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	config = function()
+		-- calling `setup` is optional for customization
+		require('fzf-lua').setup {
+			fzf_opts = {
+				['--cycle'] = true,
+			},
+			fzf_colors = true,
+			file_icon_padding = ' ',
+			winopts = {
+				title_flags = false,
+				border = 'single',
+				on_create = function()
+					vim.keymap.set(
+						't',
+						'<C-r>',
+						[['<C-\><C-N>"'.nr2char(getchar()).'pi']],
+						{ expr = true, buffer = true }
+					)
+				end,
+				width = 0.9,
+			},
+
+			files = {
+				formatter = 'path.filename_first',
+				prompt = 'file ',
+				title = false,
+				cwd_prompt = false,
+				header = false,
+				git_icons = false,
+				winopts = {
+					title = false,
+					height = 0.35,
+					width = 0.35,
+					preview = {
+						hidden = 'hidden',
+					},
+				},
+			},
+			grep = {
+				header = false,
+				prompt = 'grep ',
+				title = false,
+				rg_glob = true,
+				glob_flag = '--iglob',
+				glob_separator = '%s%-%-',
+				winopts = {
+					title = false,
+				},
+			},
+
+			keymap = {
+				builtin = {
+					['<c-d>'] = 'preview-page-down',
+					['<c-u>'] = 'preview-page-up',
+				},
+			},
+		}
+
+		local builtin = require 'fzf-lua'
+		vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+		vim.keymap.set('n', '<leader>sf', builtin.global, { desc = '[S]earch [F]iles' })
+		vim.keymap.set('n', '<leader>st', builtin.treesitter, { desc = '[S]earch current [T]reesitter symbols' })
+		vim.keymap.set('n', '<leader>sw', builtin.grep_cword, { desc = '[S]earch current [W]ord' })
+		vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+		vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+		vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+	end,
+}
--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -13,58 +13,27 @@ return {
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 				callback = function(event)
-					local map = function(keys, func, desc, mode)
+					local map = function(keys, func, desc)
 						-- helper to easily define mappings for LSP items
-						mode = mode or "n"
-						vim.keymap.set(mode, keys, func,
+						-- only sets mappings to Normal mode
+						vim.keymap.set("n", keys, func,
 							{ buffer = event.buf, desc = "LSP: " .. desc })
 					end
 
-					-- Rename the variable under your cursor.
-					--  Most Language Servers support renaming across files, etc.
 					map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
-
-					-- Execute a code action, usually your cursor needs to be on top of an error
-					-- or a suggestion from your LSP for this to activate.
-					map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
-
-					-- Find references for the word under your cursor.
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
-					-- Jump to the implementation of the word under your cursor.
-					--  Useful when your language has ways of declaring types without an actual implementation.
-					map("gri", require("telescope.builtin").lsp_implementations,
-						"[G]oto [I]mplementation")
-
-					-- Jump to the definition of the word under your cursor.
-					--  This is where a variable was first declared, or where a function is defined, etc.
-					--  To jump back, press <C-t>.
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
-					-- WARN: This is not Goto Definition, this is Goto Declaration.
-					--  For example, in C this would take you to the header.
-					map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
-					-- Fuzzy find all the symbols in your current document.
-					--  Symbols are things like variables, functions, types, etc.
-					map("gO", require("telescope.builtin").lsp_document_symbols,
-						"Open Document Symbols")
-
-					-- Fuzzy find all the symbols in your current workspace.
-					--  Similar to document symbols, except searches over your entire project.
-					map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols,
-						"Open Workspace Symbols")
-
+					map("ga", vim.lsp.buf.code_action, "[G]oto Code [A]ction")
+					map("gr", require("fzf-lua").lsp_references, "[G]oto [R]eferences")
+					map("gri", require("fzf-lua").lsp_implementations, "[G]oto [I]mplementation")
+					map("gd", function() require("fzf-lua").lsp_definitions { jump1 = true } end, "[G]oto [D]efinition") --  To jump back, press <C-t>.
+					map("gD", require("fzf-lua").lsp_declarations, "[G]oto [D]eclaration")
 					-- Jump to the type of the word under your cursor.
 					--  Useful when you're not sure what type a variable is and you want to see
 					--  the definition of its *type*, not where it was *defined*.
-					map("grt", require("telescope.builtin").lsp_type_definitions,
-						"[G]oto [T]ype Definition")
-
+					map("gt", require("fzf-lua").lsp_typedefs, "[G]oto [T]ype Definition")
 					-- Manual formatting keymap
-					map("<leader>f", function()
+					map("<leader>ff", function()
 						vim.lsp.buf.format({ async = true })
-						vim.notify('File formatted.', vim .log.levels.INFO)
+						vim.notify('File formatted.', vim.log.levels.INFO)
 					end, "[F]ormat buffer")
 
 					-- Toggle global diagnostics (linting)
@@ -79,6 +48,22 @@ return {
 								vim.log.levels.INFO)
 						end
 					end, "[T]oggle [D]iagnostics")
+
+					-- The following code creates a keymap to toggle inlay hints in your
+					-- code, if the language server you are using supports them
+					--
+					-- This may be unwanted, since they displace some of your code
+					if
+					    client
+					    and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
+					then
+						map("<leader>th", function()
+							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({
+								bufnr =
+								    event.buf
+							}))
+						end, "[T]oggle Inlay [H]ints")
+					end
 
 					-- <Experimental - not currently in use>
 					-- Toggle diagnostics (cycling through different levels)
@@ -179,22 +164,6 @@ return {
 					-- 		end,
 					-- 	})
 					-- end
-
-					-- The following code creates a keymap to toggle inlay hints in your
-					-- code, if the language server you are using supports them
-					--
-					-- This may be unwanted, since they displace some of your code
-					if
-					    client
-					    and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
-					then
-						map("<leader>th", function()
-							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({
-								bufnr =
-								    event.buf
-							}))
-						end, "[T]oggle Inlay [H]ints")
-					end
 				end,
 			})
 
