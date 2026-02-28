c81ceb2a1a28c253dd1cf49fdc449c92b8211cad
JavonTeo <javonteo@hotmail.com>
2026-03-01 00:20:42 +0800

    Add nvim treesitter context

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -2,53 +2,67 @@ return {
 	-- run :checkhealth treesitter to check:
 	--   1. requirements are all OK
 	--   2. expected languages are listed under installed
-	'nvim-treesitter/nvim-treesitter',
-	dependencies = {
-		'nvim-treesitter/nvim-treesitter-textobjects',
+	{
+		'nvim-treesitter/nvim-treesitter',
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter-textobjects',
+		},
+		lazy = false,
+		build = ':TSUpdate',
+		branch = 'main',
+		pin = true,
+		config = function()
+			local parsers = {
+				'lua',
+				'python',
+				'javascript',
+				'css',
+				'html',
+				'typescript',
+				'tsx',
+				'vue',
+				'vimdoc',
+				'vim',
+				'regex',
+				'dockerfile',
+				'json',
+				'go',
+				'yaml',
+				'markdown',
+				'markdown_inline',
+				'bash',
+				'c',
+				'cpp',
+			}
+			require('nvim-treesitter').install(parsers)
+
+			vim.api.nvim_create_autocmd('FileType', {
+				pattern = parsers,
+				callback = function(args)
+					-- 1. Enables highlighting
+					vim.treesitter.start()
+					-- 2. Enables treesitter-based folding
+					vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+					vim.wo[0][0].foldmethod = 'expr'
+					-- 3. Enables treesitter-based indentation
+					vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+
+			-- Ensure all folds are open on file entry
+			vim.opt.foldlevel = 99
+		end,
 	},
-	lazy = false,
-	build = ':TSUpdate',
-	branch = 'main',
-	pin = true,
-	config = function()
-		local parsers = {
-			'lua',
-			'python',
-			'javascript',
-			'css',
-			'html',
-			'typescript',
-			'tsx',
-			'vue',
-			'vimdoc',
-			'vim',
-			'regex',
-			'dockerfile',
-			'json',
-			'go',
-			'yaml',
-			'markdown',
-			'markdown_inline',
-			'bash',
-			'c',
-			'cpp'
-		}
-		require('nvim-treesitter').install(parsers)
-
-		vim.api.nvim_create_autocmd('FileType', {
-			pattern = parsers,
-			callback = function (args) 
-				-- 1. Enables highlighting
-				vim.treesitter.start()
-				-- 2. Enables treesitter-based folding
-				vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-				vim.wo[0][0].foldmethod = 'expr'
-				-- 3. Enables treesitter-based indentation
-				vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-			end,
-		})
-
-		-- Ensure all folds are open on file entry
-		vim.opt.foldlevel = 99
-	end
+	-- {
+	-- 	'nvim-treesitter/nvim-treesitter-context',
+	-- 	config = function ()
+	-- 		require('treesitter-context').setup{
+	-- 			enable = false, -- re-enable if desired
+	-- 			multiwindow = true,
+	-- 			max_lines = 6,
+	-- 			trim_scope = 'outer',
+	-- 			mode = 'topline',
+	-- 		}
+	-- 	end
+	-- },
 }
